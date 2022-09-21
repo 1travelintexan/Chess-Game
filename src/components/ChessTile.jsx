a7cfd073aac01977f30f9a0dc5cc590c6b023964
Joshua George <horheyinc8@gmail.com>
2022-09-21 07:38:34 +0200

    fonts, and pawns added to game board

--- src/components/ChessTile.jsx
+++ src/components/ChessTile.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-function ChessTile() {
-  return <div>ChessTile</div>;
+function ChessTile({ white, piece }) {
+  console.log(piece);
+  return (
+    <span className={`tile ${white}`}>
+      {piece && <img src={piece} alt="piece" className="piece" />}
+    </span>
+  );
 }
 
 export default ChessTile;
--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,14 +1,47 @@
 import React from "react";
+import ChessTile from "./ChessTile";
+import whitePawn from "../images/pawn_w.png";
+import blackPawn from "../images/pawn_b.png";
 
 function Chessboard() {
   let chessboard = [];
+  let pieces = [
+    //white pawns
+    { image: whitePawn, x: "a", y: 2 },
+    { image: whitePawn, x: "b", y: 2 },
+    { image: whitePawn, x: "c", y: 2 },
+    { image: whitePawn, x: "d", y: 2 },
+    { image: whitePawn, x: "e", y: 2 },
+    { image: whitePawn, x: "f", y: 2 },
+    { image: whitePawn, x: "g", y: 2 },
+    { image: whitePawn, x: "h", y: 2 },
+    //black pawns
+    { image: blackPawn, x: "a", y: 7 },
+    { image: blackPawn, x: "b", y: 7 },
+    { image: blackPawn, x: "c", y: 7 },
+    { image: blackPawn, x: "d", y: 7 },
+    { image: blackPawn, x: "e", y: 7 },
+    { image: blackPawn, x: "f", y: 7 },
+    { image: blackPawn, x: "g", y: 7 },
+    { image: blackPawn, x: "h", y: 7 },
+  ];
   let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
   for (let j = 8; j >= 1; j--) {
     for (let i = 0; i < letters.length; i++) {
-      chessboard.push(`${letters[i]}${j}`);
+      //set image to undefined and if the coordinates are the same, then update value. Then push to chessboard array with image value
+      let image = undefined;
+      pieces.forEach((piece) => {
+        if (piece.x === letters[i] && piece.y === j) {
+          image = piece.image;
+        }
+      });
+      //push object with coordinates and image properties to map over later
+      chessboard.push({ place: `${letters[i]}${j}`, image });
     }
   }
+
   console.log(chessboard);
   return (
     <div id="chess-page">
@@ -20,12 +53,15 @@ function Chessboard() {
         </div>
         <div id="chessboard">
           {chessboard.map((e, i) => {
-            if (i % 2 === 0 && e[1] % 2 === 0) {
-              return <span className="tile white"></span>;
-            } else if (i % 2 !== 0 && e[1] % 2 !== 0) {
-              return <span className="tile white"></span>;
+            if (
+              (i % 2 === 0 && e.place[1] % 2 === 0) ||
+              (i % 2 !== 0 && e.place[1] % 2 !== 0)
+            ) {
+              //white tiles with piece image added
+              return <ChessTile white={"white"} piece={e.image} />;
             } else {
-              return <span className="tile "></span>;
+              //black tiles with piece image added
+              return <ChessTile piece={e.image} />;
             }
           })}
         </div>
